cb1e99a1abee7dba1537df45fb06d13e70bfa721
sbn raju <simhardrinagaraju@gmail.com>
2024-01-08 22:41:28 +0530

    Update file

---
+++ app.js
@@ -0,0 +1,7 @@
+let navArray = document.querySelectorAll(".nav-list-items");
+navArray = Array.from(navArray).splice(1,10);
+navArray.forEach(ele =>{
+    ele.addEventListener("mouseenter",()=>{
+            
+    });
+});
